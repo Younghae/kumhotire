0e578fce985d4fea2e2c1116427d0dfc04c0bebe
leegyurye <lgr0813@naver.com>
2023-08-31 16:00:51 +0900

    imput page layout

--- app/input/page.tsx
+++ app/input/page.tsx
@@ -31,9 +31,80 @@ export default function Input() {
         </Grid>
         <Grid>
           <Grid>
-            <Item>2</Item>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              <Grid item xs={3}>
+                <Item>대분류</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>생산성 향상</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>수립일</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>부문</Item>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              <Grid item xs={3}>
+                <Item>중분류</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>Scrap 절감</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>시작일</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>팀</Item>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              <Grid item xs={3}>
+                <Item>소분류</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>Material(4M)</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>완료일</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>사번</Item>
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justifyContent="center"
+              alignItems="center">
+              <Grid item xs={3}>
+                <Item>Idea명</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>""</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>효과금액 발생 기준일</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>작성자</Item>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid container>
+          <Grid container alignItems="center">
             <Grid item xs={4}>
               <Item>현황 및 문제점</Item>
             </Grid>
@@ -41,7 +112,7 @@ export default function Input() {
               <Item>
                 <TextField
                   id="filled-basic"
-                  label="현황 및 문제점"
+                  label="현황 및 문제점을 작성해주세요."
                   variant="filled"
                   fullWidth
                   size="small"
@@ -52,21 +123,82 @@ export default function Input() {
               <Item>Idea 개요(방안)</Item>
             </Grid>
             <Grid item xs={8}>
-              <Item>2-2</Item>
+              <Item>
+                <TextField
+                  id="filled-basic"
+                  label="Idea 개요(방안)을 작성해주세요."
+                  variant="filled"
+                  fullWidth
+                  size="small"
+                />
+              </Item>
             </Grid>
             <Grid item xs={4}>
               <Item>선결과제</Item>
             </Grid>
             <Grid item xs={8}>
-              <Item>2-2</Item>
+              <Item>
+                <TextField
+                  id="filled-basic"
+                  label="선결과제를 작성해주세요."
+                  variant="filled"
+                  fullWidth
+                  size="small"
+                />
+              </Item>
             </Grid>
           </Grid>
         </Grid>
         <Grid>
-          <Item>3</Item>
+          <Grid>
+            <Item>효과파악</Item>
+          </Grid>
+          <Grid>
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                <Item>예상 효과 금액</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>""</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>효과 기간</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>""</Item>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid>
+            <Grid container direction="row">
+              <Grid item xs={3}>
+                <Item>투자비</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>""</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>순수 효과 금액</Item>
+              </Grid>
+              <Grid item xs={3}>
+                <Item>""</Item>
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid>
-          <Item>4</Item>
+          <Grid>
+            <Item>산출근거</Item>
+          </Grid>
+          <Grid>
+            <Item>개선 전</Item>
+          </Grid>
+          <Grid>
+            <Item>개선 후</Item>
+          </Grid>
+          <Grid>
+            <Item>종합</Item>
+          </Grid>
         </Grid>
       </Grid>
     </Box>
