2bf4561efc645be9fd6f6bfa729fa0540b5ed34c
leegyurye <lgr0813@naver.com>
2023-09-06 12:11:10 +0900

    update

--- app/login_gyurye/page.tsx
+++ app/login_gyurye/page.tsx
@@ -8,6 +8,9 @@ import Image from "next/image";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import Typography from "@mui/material/Typography";
 
 const Item = styled(Box)(({ theme }) => ({
   border: "1px solid",
@@ -21,6 +24,8 @@ const Item = styled(Box)(({ theme }) => ({
 }));
 
 const language = [{ label: "한국어" }, { label: "영어" }];
+
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 const Login_Gyurye = () => {
   return (
@@ -62,11 +67,144 @@ const Login_Gyurye = () => {
           </Grid>
         </Grid>
         <Grid container>
-          <Grid item xs={5}>
-            <Item>로그인</Item>
+          <Grid
+            item
+            xs={5}
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center">
+            <Grid>
+              <Item>
+                <Box
+                  sx={{
+                    color: "#EF0010",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  T
+                </Box>
+                <Box
+                  sx={{
+                    color: "#4A4A49",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  CI &nbsp;
+                </Box>
+                <Box
+                  sx={{
+                    color: "#EF0010",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  T
+                </Box>
+                <Box
+                  sx={{
+                    color: "#4A4A49",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  otal &nbsp;
+                </Box>
+                <Box
+                  sx={{
+                    color: "#EF0010",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  M
+                </Box>
+                <Box
+                  sx={{
+                    color: "#4A4A49",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  onitoring &nbsp;
+                </Box>
+                <Box
+                  sx={{
+                    color: "#EF0010",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  S
+                </Box>
+                <Box
+                  sx={{
+                    color: "#4A4A49",
+                    display: "inline",
+                    fontWeight: "bold",
+                    fontSize: 25,
+                  }}>
+                  ystem &nbsp;
+                </Box>
+              </Item>
+            </Grid>
+            <Grid container justifyContent="center" alignItems="center">
+              <Grid>
+                <Grid>
+                  <TextField label={"ID"} id="ID" margin="dense" />
+                </Grid>
+                <Grid>
+                  <TextField label={"PASSWORD"} id="PASSWORD" margin="dense" />
+                </Grid>
+              </Grid>
+              <Grid sx={{ marginLeft: 1 }}>
+                <Button
+                  variant="contained"
+                  color="error"
+                  sx={{ width: "150px", height: "125px", fontSize: "1.8rem" }}>
+                  LOGIN
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid>
+                <Checkbox defaultChecked color="error" />
+                <Typography
+                  sx={{
+                    color: "#4A4A49",
+                    display: "inline",
+                    fontSize: 15,
+                  }}>
+                  아이디저장
+                </Typography>
+              </Grid>
+              <Grid>
+                <Item>신뢰 Site 추가</Item>
+              </Grid>
+            </Grid>
+            <Grid>
+              <Item>문구2</Item>
+            </Grid>
+            <Grid>
+              <Item>버튼2개</Item>
+            </Grid>
           </Grid>
           <Grid item xs={7}>
-            <Item>그림 들어가는 곳</Item>
+            <Item>
+              <Image
+                src="/kumhoimg.jpg"
+                width={"0"}
+                height={"0"}
+                sizes="(max-width: 768px) 100%, (max-width: 1200px) 100%"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                }}
+                alt="kumhotire image"
+              />
+            </Item>
           </Grid>
         </Grid>
         <Grid>
