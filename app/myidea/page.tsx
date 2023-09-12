dcafc777c788f47c1b6c92407c1348b8778dd4e5
leegyurye <lgr0813@naver.com>
2023-09-12 16:29:58 +0900

    update

--- app/myidea/page.tsx
+++ app/myidea/page.tsx
@@ -1,23 +1,16 @@
 "use client";
 
 import * as React from "react";
-import { useState } from "react";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 
 export default async function MyIdea() {
   const getMyIdea = await fetch("http://localhost:8000/MyIdea");
   const MyIdeadata = await getMyIdea.json();
   console.log(MyIdeadata);
 
-  const [MyIdea, setMyIdea] = useState([]);
-  setMyIdead(MyIdea.MyIdea);
-
   return (
     <Container maxWidth="xl" sx={{ border: "1px solid" }}>
-      {MyIdeadata.map((item) => (
-        <Typography variant="h1">{item.Date}</Typography>
-      ))}
+      <div>이규례</div>
     </Container>
   );
 }
