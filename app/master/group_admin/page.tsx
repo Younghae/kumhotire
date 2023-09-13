4c0bb03f8dd03315e2de171919d94913f1c75e67
jonathen <jonathan@globallinkers.co.kr>
2023-09-13 20:44:24 +0900

    Small change

--- app/master/group_admin/page.tsx
+++ app/master/group_admin/page.tsx
@@ -1,33 +1,30 @@
 "use client";
 
-import { Post }  from "@/lib/types"
+import { Post } from "@/lib/types";
 import { useState, useEffect } from "react";
 
-
 export default function Group_admin() {
-const [data,setData] = useState<Post[]>([]);
+  const [data, setData] = useState<Post[]>([]);
 
-useEffect(() => {
-  async function fetchData() {
-    const response = await fetch("http://localhost:8000/User")
-    const data = await response.json();
-    setData(data);
-  }
-fetchData()
-},[])
-
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:8000/User");
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div >
+    <div>
       <h1>이름</h1>
       <ul>
         {data.map((post) => (
           <div>
-          <li key={post.id}>{post.Name}</li>
+            <li key={post.id}>{post.name}</li>
           </div>
         ))}
       </ul>
     </div>
   );
-
-        }
+}
